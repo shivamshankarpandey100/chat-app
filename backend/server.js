0bb0cb2c74383f14265c93c2f6c8993031233849
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-05-30 01:10:34 +0530

    :lock: The chat app Programing Start now :lock:

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    //root route http://localhost:5000/
+    res.send("Hello World");
+});
+app.listen(5000,()=>console.log("Server Running on Port 5000"));
