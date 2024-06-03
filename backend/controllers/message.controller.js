40356b56748bd48e9e68c37efe06e15997db22cf
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-03 21:40:16 +0530

    The user controle send and get messeges is workin and indivisualy get the messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,6 +27,18 @@ export const sendMessage = async (req, res) => {
         if(newMessage){
             conversation.messages.push(newMessage._id);
         }
+        // socket io functionality will be added here
+
+
+        // await conversation.save();
+        // await newMessage.save();
+
+        // this will run in parallel
+        await Promise.all([conversation.save(),newMessage.save()]);
+
+
+
+
 
         res.status(201).json(newMessage);
 
@@ -35,3 +47,31 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({error:"Internal Server Error"});
     }
 };
+
+
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id:userToChatId}=req.params;
+        const senderId=req.user._id;
+
+        const conversation =await Conversation.findOne({
+            participants:{ $all:[senderId,userToChatId]},
+        }).populate("messages");// not raferance but actual messages
+
+
+
+        if(!conversation){
+            return res.status(200).json([]);
+        }
+
+        const messages=conversation.messages;
+
+        res.status(200).json(messages);
+
+
+    } catch (error) {
+        console.log("Error in getMessages controller : ",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
