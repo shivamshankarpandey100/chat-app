e452114138c33b7046296d847eef880338958f7e
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-07 21:27:22 +0530

    The login page and Signup page devloped but stil not working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 import Login from './pages/login/Login.jsx';
+import SignUp from './pages/signup/SignUp.jsx';
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login/>
+      <SignUp/>
     </div>
   )
 }
