8ede91b5dacf8477301377c047ddda70be02160f
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-09 21:20:49 +0530

    the conversatation and conversations block created

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import { IoSearchSharp } from "react-icons/io5";
 const SearchInput = () => {
   return (
     <form className='flex items-center gap-2'>
@@ -8,7 +8,9 @@ const SearchInput = () => {
 				className='input input-bordered rounded-full' />
 
 <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-				Icon
+        <IoSearchSharp className='w-6 h-6 outline-none' />
+
+
 			</button>
     </form>
   )
