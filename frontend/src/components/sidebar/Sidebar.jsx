dfd71330345411d2a9de21964fc12578ff5c759a
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-14 21:22:12 +0530

    Logout button added in side bar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SearchInput from './SearchInput.jsx'
 import Conversations from './Conversations.jsx'
+import LogoutButton from './LogoutButton.jsx'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <SearchInput/>
         <div className='divider px-3'></div>
         <Conversations />
