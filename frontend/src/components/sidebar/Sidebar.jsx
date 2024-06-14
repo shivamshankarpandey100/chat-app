21d806b60fe57fcf675d240545e34e8a92922253
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-14 21:09:16 +0530

    The conbversatation sidebar  withe user created

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SearchInput from './SearchInput.jsx'
+import Conversations from './Conversations.jsx'
 
 const Sidebar = () => {
   return (
     <div>
         <SearchInput/>
         <div className='divider px-3'></div>
+        <Conversations />
         {/* <Conversations />
 		<LogoutButton /> */}
     </div>
