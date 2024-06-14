a8c7060d0444cd08a92f742e8acd5b3825a2e248
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-14 21:25:36 +0530

    the message container is created

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MessageContainer = () => {
+  return (
+    <div className='md:min-w-[450px] flex flex-col'>
+
+        
+    </div>
+  )
+}
+
+export default MessageContainer
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -14,4 +14,23 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
+
+// //started code for this file
+// import React from 'react'
+// import SearchInput from './SearchInput.jsx'
+// import Conversations from './Conversations.jsx'
+// import LogoutButton from './LogoutButton.jsx'
+
+// const Sidebar = () => {
+//   return (
+//     <div className='border-r border-slate-500 p-4 flex flex-col'>
+//         <SearchInput/>
+//         <div className='divider px-3'></div>
+//         <Conversations />
+//         <LogoutButton />
+//     </div>
+//   )
+// }
+
+// export default Sidebar;
