963345368a82ce4fb6e59f6126b8d346451c37dc
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-17 19:44:57 +0530

    2nd part completed UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css'
 import Home from './pages/home/Home.jsx';
-import Login from './pages/login/Login.jsx';
-import SignUp from './pages/signup/SignUp.jsx';
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <SignUp/> */}
       <Home/>
     </div>
   )
