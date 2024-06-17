967965118d0c615e7e722ef1e124e4e55d23b89c
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-17 20:48:35 +0530

    the Signup and check box work on progress

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,35 +1,34 @@
 import React from 'react'
 
-const GenderCheckbox = () => {
-  return (
-    <div className='flex'>
-        <div className='form-control'>
-        <label className={`label gap-2 cursor-pointer `}>
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
+	return (
+		<div className='flex'>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
 					<span className='label-text'>Male</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
-						
+						checked={selectedGender === "male"}
+						onChange={() => onCheckboxChange("male")}
 					/>
 				</label>
-        </div>
-        <div className='form-control' >
-        <label className={`label gap-2 cursor-pointer `}>
+			</div>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
 					<span className='label-text'>Female</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
-						
+						checked={selectedGender === "female"}
+						onChange={() => onCheckboxChange("female")}
 					/>
 				</label>
-        </div>
-    </div>
-  )
-}
-
+			</div>
+		</div>
+	);
+};
 export default GenderCheckbox;
-
-
 
 // //the Starter code for this component is:
 // import React from 'react'
