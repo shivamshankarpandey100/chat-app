05dc32495d9d53741559af196b6edfd0eeb6d888
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-07-14 00:46:33 +0530

    login Problem is solve

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -7,6 +7,9 @@ const useLogin = () => {
    const {setAuthUser}= useAuthContext();
 
   const login = async (username,password) => {
+
+    const success = handleInputErrors(username,password);
+    if(!success) return;
     setLoading(true);
     try {
         const res=await fetch('/api/auth/login',{
@@ -32,3 +35,14 @@ const useLogin = () => {
 }
 
 export default useLogin;
+
+
+function handleInputErrors(username,password){
+    if (!username || !password ){
+        toast.error("Please fill in all fields");
+		return false;
+    }
+    
+
+	return true;
+}
