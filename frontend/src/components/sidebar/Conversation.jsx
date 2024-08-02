34dc639712b2f1c1e3edf054d2686e911b403b66
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-08-02 16:28:01 +0530

    the conversatations not load on the page

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 
-const Conversation = () => {
+const Conversation = ({conversation,lastIdx,emoji}) => {
   return (
     <>
     <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
         <div className='avatar online'>
             <div className='w-12 rounded-full'>
-                <img src="avtar url here" alt="user avatar" />
+                <img src={conversation.profilePic} alt="user avatar" />
             </div>
         </div>
 
           <div className='flex flex-col flex-1'>
             <div className='flex gap-3 justify-between'>
-              <p className='font-bold text-gray-200'>jhon doe</p>
-              <span className='text-xl'>🦘</span>
+              <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+              <span className='text-xl'>{emoji}</span>
             </div>
           </div>
 
     </div>
-    <div className='divider my-0 py-0 h-1'/>
+    {/* <div className='divider my-0 py-0 h-1'/> */}
+    {!lastIdx && <div className='divider my-0 py-0 h-1'/>}
     </>
   )
 }
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Conversation from './Conversation.jsx'
+import useGetConversations from '../../hooks/useGetConversations.js';
+import { getRandomEmoji } from '../../utils/emojis.js';
 
 const Conversations = () => {
+  const {loading,conversations}= useGetConversations();
+  // console.log("CONVERSATION:",conversations); //CHECK THE CONVERSATATIONS ARE GET FROM BACKEND OR NOT
+  
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+        {conversations.map((conversation,idx)=>(
+          <Conversation 
+              key={conversation._id}
+              conversation={conversation}
+              emoji={getRandomEmoji()}
+              lastIdx={idx===conversations.length-1}
+          />
+        ))}
+  {loading ? <span className='loading loading-spinner mx-auto'></span>:null}
 
     </div>
   )
